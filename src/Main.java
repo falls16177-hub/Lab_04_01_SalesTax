a38636bc2a76d9641882e9a9cbe18dce4aeaf03e
ace <falls.16177@gmail.com>
2025-10-01 13:18:49 -0400

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("What is the price of your item? ");
-        int itemprice = 12;
-        double salesTax = .05;
+        int ItemPrice = 50;
+        double SalesTax = .05;
 
-        double salesTotal = itemprice * salesTax;
+        double SalesTotal = ItemPrice * SalesTax;
 
-        System.out.println("The sales price is $" + salesTotal);
+        System.out.println("The sales price is $" + SalesTotal);
 
     }
 }
